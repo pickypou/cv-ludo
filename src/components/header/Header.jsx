412c53f9bf16fb3f3847f99f108288b383c79e70
Ludovic SPYSSCHAERT <ludovicspysschaert@air-de-ludovic.home>
2022-10-02 11:29:33 +0200

    creation du header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './header.css'
+import ludo from "./ludo-2.jpg";
+
+
+
+export default function header() {
+  return (
+    <div className="header" id="header">
+      <h1 className="title-header">spysschaert ludovic</h1>
+      <img src={ludo} alt="photo spysschaert ludovic" className="fluid  img-ludo" />
+      <h3 className="dev-web">developper web</h3>
+    </div>
+  );
+}
